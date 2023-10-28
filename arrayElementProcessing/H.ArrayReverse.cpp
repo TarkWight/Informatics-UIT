a501f823d8f28664326ceba5eb2528237f3624d8
Arsentiy <82136438+TarkWight@users.noreply.github.com>
2023-10-28 19:12:54 +0700

    [Create]

---
+++ arrayElementProcessing/H.ArrayReverse.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+
+int main() {
+    int numberOfElements;
+    std::cin >> numberOfElements;
+    
+    std::vector<int> array(numberOfElements);
+    for (int i = 0; i < array.size(); ++i) {
+        std::cin >> array[i];
+    }
+    
+
+    for (int i = array.size() - 1; i >= 0; --i) {
+        std::cout << array[i] << ' ';
+    }
+
+    return 0;
+}

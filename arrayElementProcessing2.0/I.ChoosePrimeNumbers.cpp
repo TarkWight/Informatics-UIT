35cb6b169889be2bc5ecb0fcb00be653b11d4e7c
Arsentiy <82136438+TarkWight@users.noreply.github.com>
2023-11-05 15:40:37 +0700

    Update

--- arrayElementProcessing2.0/I.ChoosePrimeNumbers.cpp
+++ arrayElementProcessing2.0/I.ChoosePrimeNumbers.cpp
@@ -1,1 +1,42 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
 
+bool isPrimeNumber(int number) {
+    if (number == 1) {
+        return false;
+    } else {
+        for (int i = 2; i <= sqrt(number); i++) {
+            if (number % i == 0) {
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+int main() {
+    int size;
+    std::cin >> size;
+    
+    std::vector<int> inputArray(size);
+    std::vector<int> outputArray;
+    
+    for (auto& element : inputArray) {
+        std::cin >> element;
+        if (isPrimeNumber(element)) {
+            outputArray.push_back(element);
+        }
+    }
+    
+    if (outputArray.empty()) {
+        std::cout << 0;
+    } else {
+        for (auto& element : outputArray) {
+            std::cout << element << ' ';
+        }
+    }
+    
+    
+    return 0;
+}

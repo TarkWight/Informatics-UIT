2d9530f4747834dd6a3ccdcefda7bcfbba81d9c1
Arsentiy <82136438+TarkWight@users.noreply.github.com>
2023-11-05 18:46:23 +0700

    Update

--- arrayElementProcessing2.0/K.ArrayCompression.cpp
+++ arrayElementProcessing2.0/K.ArrayCompression.cpp
@@ -1,1 +1,34 @@
+#include <iostream>
+#include <vector>
 
+
+int main() {
+    int size, counter = 0;
+    std::cin >> size;
+    
+    std::vector<int> inputArray(size);
+    std::vector<int> outputArray(size);
+    
+    
+    for (int i = 0; i < size; i++) {
+        std::cin >> inputArray[i];
+        
+        if (inputArray[i] == 0) {
+            counter ++;
+        } else {
+            outputArray[i - counter] = inputArray[i];
+        }
+    }
+        
+    
+    for (int i = size - counter; i < size; i++) {
+        outputArray[i] = 0;
+    }
+    
+    
+    for (auto& element : outputArray) {
+        std::cout << element << " ";
+    }
+ 
+    return 0;
+}

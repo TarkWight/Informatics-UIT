9e03b14552779b657adabe63ddab3a5c457536e2
Arsentiy <82136438+TarkWight@users.noreply.github.com>
2023-11-06 20:10:47 +0700

    Update

--- StringsBasic/D.WordIsPalindrome.cpp
+++ StringsBasic/D.WordIsPalindrome.cpp
@@ -1,1 +1,20 @@
+#include <iostream>
+#include <string>
 
+int main() {
+    std::string inputString;
+    std::cin >> inputString;
+    
+    bool check = true;
+    
+    for (unsigned long i = 0; i < inputString.length() ; i++) {
+        if (inputString[i] != inputString[inputString.length() - i - 1]) {
+            check = false;
+            break;
+        }
+    }
+    
+    check ? std::cout << "yes" : std::cout << "no";
+    
+    return 0;
+}

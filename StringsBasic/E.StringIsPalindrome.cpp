934cbd05a1617c3de74bc6d1548a591c029d8bce
Arsentiy <82136438+TarkWight@users.noreply.github.com>
2023-11-06 20:33:22 +0700

    Update

--- StringsBasic/E.StringIsPalindrome.cpp
+++ StringsBasic/E.StringIsPalindrome.cpp
@@ -1,1 +1,28 @@
+#include <iostream>
+#include <string>
 
+int main() {
+    std::string inputString;
+    std::getline(std::cin, inputString);
+    
+    bool check = true;
+    int length = static_cast<int>(inputString.length());
+    for (int i = 0; i < length; i++) {
+        if (inputString[i] == ' ') {
+            inputString.erase(i, 1);
+            length--;
+            i--;
+        }
+    }
+    
+    for (unsigned long i = 0; i < length; i++) {
+        if (inputString[i] != inputString[length - i - 1]) {
+            check = false;
+            break;
+        }
+    }
+    
+    check ? std::cout << "yes" : std::cout << "no";
+    
+    return 0;
+}

3e5cf162514335dced03a3ef24da6fb8abc03d78
Arsentiy <82136438+TarkWight@users.noreply.github.com>
2023-11-06 13:48:40 +0700

    Update

--- arrayElementProcessing2.0/H.RightShiftKM.cpp
+++ arrayElementProcessing2.0/H.RightShiftKM.cpp
@@ -1,1 +1,31 @@
+#include <iostream>
+#include <vector>
 
+int main() {
+    int size, startingShiftElement, finitShiftElement, shift;
+    std::cin >> size;
+
+    std::vector<int> array(size);
+    for (auto& element : array) {
+        std::cin >> element;
+    }
+
+    std::cin >> startingShiftElement >> finitShiftElement >> shift;
+
+    for (int i = 0; i < shift; i++) {
+        int j = finitShiftElement - 1;
+        int temp = array[j];
+
+        while (j > startingShiftElement - 1) {
+            array[j] = array[j - 1];
+            j--;
+        }
+        array[startingShiftElement - 1] = temp;
+    }
+
+    for (auto& element : array) {
+        std::cout << element << ' ';
+    }
+
+    return 0;
+}

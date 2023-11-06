6f19c3719080d0006356e8e171683928df22533b
Arsentiy <82136438+TarkWight@users.noreply.github.com>
2023-11-06 23:13:50 +0700

    Update

--- StringsBasic/F.HowManyWords.cpp
+++ StringsBasic/F.HowManyWords.cpp
@@ -1,1 +1,34 @@
+#include <iostream>
+#include <string>
 
+
+int countWords(const std::string& string, const std::string& split) {
+  bool check = false;
+  int counter = 0;
+
+  for (char symbol : string) {
+    if (split.find(symbol) != std::string::npos) {
+      if (check) {
+        check = false;
+      }
+    } else {
+      if (!check) {
+        check = true;
+        counter++;
+      }
+    }
+  }
+
+  return counter;
+}
+
+
+int main() {
+    std::string inputString;
+    std::getline(std::cin, inputString);
+    std::string split = " ";
+
+    std::cout << countWords(inputString, split) << std::endl;
+
+    return 0;
+}

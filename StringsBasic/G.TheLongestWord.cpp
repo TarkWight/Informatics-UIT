d54b94a294e4bde8f7620b1945563f811ba00430
Arsentiy <82136438+TarkWight@users.noreply.github.com>
2023-11-06 22:28:28 +0700

    Update

--- StringsBasic/G.TheLongestWord.cpp
+++ StringsBasic/G.TheLongestWord.cpp
@@ -1,1 +1,32 @@
+#include <iostream>
+#include <string>
 
+
+int main() {
+    std::string string;
+    std::getline(std::cin, string);
+    
+    int count = 0, maxCount = 0,  lastIterator = 0;
+    
+    int length = static_cast<int>(string.length());
+    for (int i = 0; i < length; i++) {
+        if (string[i] != ' ') {
+            count++;
+            if (count > maxCount) {
+                maxCount = count;
+                lastIterator = i;
+            }
+        } else {
+            count = 0;
+        }
+    }
+    
+    std::string word(maxCount, 'x');
+    for (int i = lastIterator - maxCount + 1, j = 0; j < maxCount; i++, j++) {
+        word[j] = string[i];
+    }
+    
+    std::cout << word << '\n' << maxCount;
+    
+    return 0;
+}

1186993f563c0b72c5c115eaa318aecefe67f1e0
Arsentiy <82136438+TarkWight@users.noreply.github.com>
2023-11-06 21:14:15 +0700

    Update

--- StringsBasic/K.JuliusCode.cpp
+++ StringsBasic/K.JuliusCode.cpp
@@ -1,1 +1,27 @@
+#include<string>
+#include<iostream>
 
+
+int main() {
+    std::string cipherWord;
+    std::cin >> cipherWord;
+    
+    std::string auxWord = cipherWord;
+    
+    int shift;
+    std::cin >> shift;
+    
+    int length = static_cast<int>(cipherWord.length());
+    
+    for (int i = 0; i < length; i++) {
+        cipherWord[i] = static_cast<int>(cipherWord[i]) - shift;
+        if (cipherWord[i] < 65) {
+            auxWord[i] = static_cast<int>(cipherWord[i]) + 26;
+            cipherWord[i] = auxWord[i];
+        }
+    }
+    
+    std::cout << cipherWord;
+    
+    return 0;
+}

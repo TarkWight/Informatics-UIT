2c71c5e20d3e139fccb69d8303876cfc3f5a42f7
Arsentiy <82136438+TarkWight@users.noreply.github.com>
2023-11-08 18:36:19 +0700

    Update

--- StringSlicing/E.WordCount.cpp
+++ StringSlicing/E.WordCount.cpp
@@ -1,1 +1,12 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
 
+int main() {
+    std::string inputString;
+    std::getline(std::cin, inputString);
+    
+    std::cout << std::count(inputString.begin(), inputString.end(), ' ') + 1 << std::endl;
+
+    return 0;
+}

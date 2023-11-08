6e446048ee600eb88d8d5de27a323e131a4cb097
Arsentiy <82136438+TarkWight@users.noreply.github.com>
2023-11-08 20:34:37 +0700

    Update

--- StringSlicing2.0/C.InsertingCharacters.cpp
+++ StringSlicing2.0/C.InsertingCharacters.cpp
@@ -1,1 +1,17 @@
+#include <iostream>
+#include <iostream>
 
+
+int main() {
+    std::string string;
+    std::getline(std::cin, string);
+    
+    size_t t = std::size(string);
+  
+    for (int i = 0; i < t * 2 - 2; i++) {
+        string.insert(++i, "*");
+    }
+    
+    std::cout << string;
+    return 0;
+}

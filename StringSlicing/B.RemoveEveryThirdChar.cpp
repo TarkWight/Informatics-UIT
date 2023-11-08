8b31865359ff9255c251fa9867d9ad68a7e3ffa5
Arsentiy <82136438+TarkWight@users.noreply.github.com>
2023-11-08 16:46:05 +0700

    Update

--- StringSlicing/B.RemoveEveryThirdChar.cpp
+++ StringSlicing/B.RemoveEveryThirdChar.cpp
@@ -1,1 +1,18 @@
+#include <iostream>
+#include <string>
 
+
+int main() {
+    std::string string;
+    std::getline(std::cin, string);
+   
+    if (!string.empty() && string.length() >= 2) {
+        for (int i = 1; i < string.length(); i++) {
+            if (i % 3 != 0) {
+                std::cout << string[i];
+            }
+        }
+    }
+    
+    return 0;
+}

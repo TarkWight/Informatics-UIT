e255742f2211fb66969054307c52e58664e22e67
Arsentiy <82136438+TarkWight@users.noreply.github.com>
2023-11-08 20:26:31 +0700

    Update

--- StringSlicing2.0/B.SubstringReplacement.cpp
+++ StringSlicing2.0/B.SubstringReplacement.cpp
@@ -1,1 +1,16 @@
+#include <iostream>
+#include <iostream>
 
+
+int main() {
+    std::string string;
+    std::getline(std::cin, string);
+    
+    size_t occurrence;
+    while ((occurrence = string.find('1')) != std::string::npos) {
+            string.replace(occurrence, 1, "one");
+    }
+    
+    std::cout << string;
+    return 0;
+}

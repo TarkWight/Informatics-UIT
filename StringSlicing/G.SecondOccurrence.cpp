a044d39176d66473186f7e9fc5d1236f4162b453
Arsentiy <82136438+TarkWight@users.noreply.github.com>
2023-11-08 19:02:17 +0700

    Update

--- StringSlicing/G.SecondOccurrence.cpp
+++ StringSlicing/G.SecondOccurrence.cpp
@@ -1,1 +1,26 @@
+#include <iostream>
+#include <string>
 
+
+int main() {
+    std::string string;
+    std::getline(std::cin, string);
+
+    size_t firstOccurrence = string.find('f');
+    
+    
+    if (firstOccurrence != std::string::npos) {
+        string.erase(firstOccurrence, 1);
+        
+        size_t secondOccurrence = string.find('f');
+        if (++secondOccurrence) {
+            std::cout << secondOccurrence ;
+        } else {
+            std::cout << -1;
+        }
+    } else {
+        std::cout << -2;
+    }
+
+    return 0;
+}

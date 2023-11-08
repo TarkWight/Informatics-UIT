32c4eed4fcd1ca2bb37346fdefc5b43056dc27ec
Arsentiy <82136438+TarkWight@users.noreply.github.com>
2023-11-08 18:32:51 +0700

    Update

--- StringSlicing/D.TwoHalves.cpp
+++ StringSlicing/D.TwoHalves.cpp
@@ -1,1 +1,22 @@
+#include <iostream>
+#include <string>
 
+int main() {
+    std::string inputString;
+    std::getline(std::cin, inputString);
+    
+    // Исходя их функционального подхода - минимум переменных
+    std::string resultString = inputString.substr((inputString.length() + 1) / 2) +
+                               inputString.substr(0, (inputString.length() + 1) / 2);
+    
+    /*
+    Иначе можно декомпозировать ещё больше...
+    size_t length = inputString.length();
+    size_t halfLength = (length + 1) / 2;
+
+    std::string resultString = inputString.substr(halfLength) + inputString.substr(0, halfLength);
+     */
+    std::cout << resultString << std::endl;
+
+    return 0;
+}

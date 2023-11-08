6d6313d5eff38b9bd7970318c20681f3aee11400
Arsentiy <82136438+TarkWight@users.noreply.github.com>
2023-11-08 18:20:33 +0700

    Update

--- StringSlicing/C.RearrangeTwoWords.cpp
+++ StringSlicing/C.RearrangeTwoWords.cpp
@@ -1,1 +1,16 @@
+#include <iostream>
+#include <sstream>
 
+int main() {
+    std::string inputString;
+    std::getline(std::cin, inputString);
+    
+    std::istringstream iss(inputString);
+    std::string firstWord, secondWord;
+
+    iss >> firstWord >> secondWord;
+    std::cout << secondWord << ' ' << firstWord << std::endl;
+    
+
+    return 0;
+}

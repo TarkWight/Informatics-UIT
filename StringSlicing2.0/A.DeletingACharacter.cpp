19709e0e110056f24a2a5f220bd906977d08f97b
Arsentiy <82136438+TarkWight@users.noreply.github.com>
2023-11-08 20:13:55 +0700

    Update

--- StringSlicing2.0/A.DeletingACharacter.cpp
+++ StringSlicing2.0/A.DeletingACharacter.cpp
@@ -1,1 +1,21 @@
+#include <iostream>
+#include <iostream>
 
+
+int main() {
+    std::string string;
+    std::getline(std::cin, string);
+    
+    
+    while (true) {
+        size_t occurrence = string.find('@');
+        if (occurrence != std::string::npos) {
+            string.erase(occurrence, 1);
+        } else {
+            break;
+        }
+    }
+    
+    std::cout << string;
+    return 0;
+}

ec60d87a477e6dcad452fcdbe7118cd0ee8a4fcb
Arsentiy <82136438+TarkWight@users.noreply.github.com>
2023-11-08 18:51:00 +0700

    Update

--- StringSlicing/F.FirstAndLastOccurrence.cpp
+++ StringSlicing/F.FirstAndLastOccurrence.cpp
@@ -1,1 +1,21 @@
+#include <iostream>
+#include <string>
 
+
+int main() {
+    std::string string;
+    std::getline(std::cin, string);
+
+    size_t firstOccurrence = string.find('f');
+    if (firstOccurrence != std::string::npos) {
+        
+        size_t lastOccurrence = string.rfind('f');
+        if (firstOccurrence == lastOccurrence) {
+            std::cout << firstOccurrence << std::endl;
+        } else {
+            std::cout << firstOccurrence << ' ' << lastOccurrence;
+        }
+    }
+
+    return 0;
+}

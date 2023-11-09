ed16ef7f9a96269bfca8cf4a5fb4a5595a81c39f
Arsentiy <82136438+TarkWight@users.noreply.github.com>
2023-11-09 14:26:20 +0700

    Update

--- StringSlicing2.0/D.RemovingAFragment.cpp
+++ StringSlicing2.0/D.RemovingAFragment.cpp
@@ -1,1 +1,16 @@
+#include <iostream>
+#include <string>
 
+
+int main(){
+    std::string string;
+    std::getline(std::cin,string);
+    
+    //    size_t firstOccurrence = string.find('h');
+    //    size_t lastOccurrence = string.rfind('h');
+    
+    string.erase(string.find('h'), string.rfind('h') - string.find('h') + 1);
+    
+    std::cout << string;
+    return 0;
+}

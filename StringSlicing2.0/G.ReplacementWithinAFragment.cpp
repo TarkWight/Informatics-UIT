460b3e1b7088bdfd11ea4f24cf63828ced1873cd
Arsentiy <82136438+TarkWight@users.noreply.github.com>
2023-11-11 09:14:42 +0700

    Update

--- StringSlicing2.0/G.ReplacementWithinAFragment.cpp
+++ StringSlicing2.0/G.ReplacementWithinAFragment.cpp
@@ -1,1 +1,26 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
 
+
+int main() {
+    std::string inputString;
+    std::getline(std::cin, inputString);
+    
+    size_t firstH = inputString.find('h');
+    size_t lastH = inputString.rfind('h');
+    
+    std::string betweenH = inputString.substr(firstH + 1, lastH - firstH - 1);
+    
+    size_t hToH = betweenH.find('h');
+    while (hToH != std::string::npos) {
+        betweenH.replace(hToH, 1, "H");
+        hToH = betweenH.find('h');
+    }
+    
+    inputString.replace(firstH + 1, lastH - firstH - 1, betweenH);
+    
+    std::cout << inputString << std::endl;
+    
+    return 0;
+}

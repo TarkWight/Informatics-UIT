b3de7e6460c9814e466332de4bccedb89df8a229
Arsentiy <82136438+TarkWight@users.noreply.github.com>
2023-11-11 08:12:00 +0700

    Update

--- StringSlicing2.0/E.DuplicatingAFragment.cpp
+++ StringSlicing2.0/E.DuplicatingAFragment.cpp
@@ -1,1 +1,18 @@
+#include <iostream>
+#include <string>
 
+
+int main() {
+    std::string inputString;
+    std::getline(std::cin, inputString);
+    
+    size_t firstH = inputString.find('h');
+    size_t lastH = inputString.rfind('h');
+    
+    std::string betweenH = inputString.substr(firstH + 1, lastH - firstH - 1);
+    inputString.insert(firstH + 1, betweenH);
+    
+    std::cout << inputString << std::endl;
+    
+    return 0;
+}

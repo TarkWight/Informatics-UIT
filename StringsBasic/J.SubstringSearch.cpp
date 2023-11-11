acb5a71154b2e9d2d6a1f6e0a1bbb187d6dacdd5
Arsentiy <82136438+TarkWight@users.noreply.github.com>
2023-11-11 09:34:01 +0700

    Update

--- StringsBasic/J.SubstringSearch.cpp
+++ StringsBasic/J.SubstringSearch.cpp
@@ -1,1 +1,16 @@
+#include <iostream>
+#include <string>
 
+bool isSubstring(const std::string& firstString, const std::string& secondtString) {
+    size_t found = secondtString.find(firstString);
+    return found != std::string::npos;
+}
+
+int main() {
+    std::string firstString, secondtString;
+    std::cin >> firstString >> secondtString;
+
+    isSubstring(firstString, secondtString) ? std::cout << "yes\n" : std::cout << "no\n";
+
+    return 0;
+}

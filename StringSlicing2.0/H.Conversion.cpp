523cd29ce64e59d87bfb9e04400d00de24df7c29
Arsentiy <82136438+TarkWight@users.noreply.github.com>
2023-11-11 09:27:09 +0700

    Update

--- StringSlicing2.0/H.Conversion.cpp
+++ StringSlicing2.0/H.Conversion.cpp
@@ -1,1 +1,20 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
 
+int main() {
+    std::string string;
+    std::getline(std::cin, string);
+    
+    size_t firstPosition, lastPosition;
+    std::cin >> firstPosition >> lastPosition;
+    
+    auto firstIter = string.begin() + (firstPosition - 1);
+    auto lastIter = string.begin() + (lastPosition - 1);
+    
+    std::reverse(firstIter, lastIter + 1);
+    
+    std::cout << string << std::endl;
+    
+    return 0;
+}

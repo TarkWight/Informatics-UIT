236286f1dbed84eb7ad26e327f34d11921f883b9
Arsentiy <82136438+TarkWight@users.noreply.github.com>
2023-11-12 13:16:28 +0700

    Update

--- DT_FloatAndMath/H.Hours.3.cpp
+++ DT_FloatAndMath/H.Hours.3.cpp
@@ -1,1 +1,17 @@
+#include <iostream>
+#include <cmath>
 
+int main() {
+    float angle;
+    std::cin >> angle;
+
+    float totalSeconds = angle / 360.0 * 12 * 60 * 60;
+    
+    int hours = static_cast<int>(totalSeconds / 3600);
+    int minutes = static_cast<int>((totalSeconds - hours * 3600) / 60);
+    int seconds = static_cast<int>(totalSeconds - hours * 3600 - minutes * 60);
+
+    std::cout << hours << " " << minutes << " " << seconds ;
+
+    return 0;
+}

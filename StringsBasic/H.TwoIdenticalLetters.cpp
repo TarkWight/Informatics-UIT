a50a52497fe9e755c849a96ae07408c2da1da417
Arsentiy <82136438+TarkWight@users.noreply.github.com>
2024-03-31 15:01:55 +0700

    Update H.TwoIdenticalLetters.cpp

--- StringsBasic/H.TwoIdenticalLetters.cpp
+++ StringsBasic/H.TwoIdenticalLetters.cpp
@@ -1,1 +1,23 @@
-// 
+#include<string>
+#include<iostream>
+
+
+int main() {
+    std::string inputString;
+    std::getline(std::cin, inputString);
+    
+    int length = static_cast<int>(inputString.length());
+    char symbol = '\0';
+    
+    for (int i = 0; i < length; i++) {
+        for (int j= i + 1; j < length; j++) {
+            if (inputString[i] == inputString[j]) {
+                symbol = inputString[i];
+            }
+        }
+    }
+    
+    std::cout << symbol;
+    
+    return 0;
+}
